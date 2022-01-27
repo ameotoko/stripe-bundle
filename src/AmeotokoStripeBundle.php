4553a04b245182241091a6d36fec985c9fab803e
ameotoko <andrey.vinichenko@gmail.com>
2022-01-27 18:00:19 +0100

    initial commit

---
+++ src/AmeotokoStripeBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Andrey Vinichenko <andrey.vinichenko@gmail.com>
+ */
+
+namespace Ameotoko\StripeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AmeotokoStripeBundle extends Bundle
+{
+}
